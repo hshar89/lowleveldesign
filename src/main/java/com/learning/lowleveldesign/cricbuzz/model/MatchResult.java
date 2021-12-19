eb2d3949b74b277316d53d866a7ec7efb212613c
hshar89 <harsh1996.007@gmail.com>
2021-12-19 21:42:13 +0530

    Adding match result class

---
+++ src/main/java/com/learning/lowleveldesign/cricbuzz/model/MatchResult.java
@@ -0,0 +1,9 @@
+package com.learning.lowleveldesign.cricbuzz.model;
+
+public class MatchResult {
+  int resultId;
+  int winningTeamId;
+  int manOfTheMatchPlayerId;
+  int bowlerOfTheMatchPlayerId;
+  int matchId;
+}
